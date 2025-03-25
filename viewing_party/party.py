93dde833fb971046c76c871ba6cdb69c78f7bbdc
FloritaZhao <feifeilan@hotmail.com>
2025-03-25 13:54:26 -0700

    revised wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,31 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+    else:
+        return {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movies in user_data["watchlist"]:
+        if movies["title"] == title:
+            user_data["watched"].append(movies)
+            user_data["watchlist"].remove(movies)
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
