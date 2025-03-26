e1592e5bc70dc38d08a5b0a94e11cec6f762b0fd
Ellie Alavi <elhamalavi.f@gmail.com>
2025-03-25 22:55:20 -0400

    Added new assertion in wave-01 last 2 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,71 @@
 # ------------- WAVE 1 --------------------
+#  User_data dictionary is the main data structure working with here.
+
+# Clarifying questions:
+
+
+# invalid or missing input handling
+# what is None title,genre,rating
+# what if use_data = {}
 
 def create_movie(title, genre, rating):
-    pass
+    '''
+    input title and genre as strings, ratign as number
+        MOVIE_TITLE_1 = "It Came from the Stack Trace"
+        GENRE_1 = "Horror"
+        RATING_1 = 3.5
+    output is a dictionary which has 3 key/value pairs; returns None if no title or genre or rating
+    '''
+
+    if not "title" or not "rating" or not "genre":
+        return None
+    else:
+        return {"title": title,
+                "genre": genre, 
+                "rating" : rating
+                }
+        
+
+def add_to_watched(user_data, movie):
+    '''
+    input: movie is a dictionary of 3 key/value pairs. User_data is a nested dict with 1 key ("watched") 
+    and value as a list that has a dictionary of movie(s)
+    output: function adds the movie dict information to the "watched" list of user
+    '''
+    user_data["watched"].append(movie)
+
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    '''
+    input: 2 dicts
+    output: is a dict with key as "watchlist" and value as a list; the list contains dictionaries of movies (3 key/value)
+    '''    
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+
+def watch_movie(user_data, title):
+    '''
+    output: if the movie is in the user_data watchlist, 
+    we move the movie from the "watchlist" to the "watched" list in user_data 
+    '''
+    for movies in user_data["watchlist"]:
+        if movies["title"] == title:
+            user_data["watched"].append(movies)
+            user_data["watchlist"].remove(movies)
+
+    return user_data
+
+# def test_invalid_rating():
+#     # act & assert
+#     with pytest.raises(ValueError):
+#         watch_movie(….)
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -21,3 +85,39 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+#-----------------------
+MOVIE_TITLE_1 = "It Came from the Stack Trace"
+GENRE_1 = "Horror"
+RATING_1 = 3.5
+
+movie = {
+    "title": MOVIE_TITLE_1,
+    "genre": GENRE_1,
+    "rating": RATING_1
+}
+# user_data = {
+#     "watched": [],
+#     "watchlist": []
+# }
+
+# user_data = {
+#     "watchlist": []
+# }
+
+# user_data = {
+#     "watched": [],
+# }
+
+user_data = {
+    "watched": [{
+    "title": "The Lord of the Functions: The Fellowship of the Function",
+    "genre": "Fantasy",
+    "rating": 4.8
+}],
+    "watchlist": []
+}
+
+# print(create_movie(MOVIE_TITLE_1, GENRE_1, RATING_1))
+# print(add_to_watched(user_data, movie))
+print(add_to_watchlist(user_data, movie))
+# print(watch_movie(user_data, movie))
